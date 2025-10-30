b8085ce3bf630b99b8128dedf6f35bbbf38f7685
Spindelen <erik@manboy.se>
2025-10-30 10:34:45 +0100

    added attributes to person class

--- src/main/java/se/lexicon/model/Person.java
+++ src/main/java/se/lexicon/model/Person.java
@@ -2,4 +2,9 @@ package se.lexicon.model;
 
 public class Person {
     // todo: needs completion
+private int sequencer = 0;
+private int id;
+private String firstName;
+private String lastName;
+
 }
