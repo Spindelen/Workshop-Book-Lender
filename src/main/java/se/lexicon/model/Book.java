dae4c5c281d255c3ea4406595eeb770617b4497c
Spindelen <erik@manboy.se>
2025-10-30 10:17:21 +0100

    added attributes

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -2,4 +2,9 @@ package se.lexicon.model;
 
 public class Book {
     // todo: needs completion
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+    
 }
