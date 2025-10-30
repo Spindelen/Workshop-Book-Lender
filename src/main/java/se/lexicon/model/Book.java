8a0d6e9ded7968d9ab40c23760c9a17a3e36cf07
Spindelen <erik@manboy.se>
2025-10-30 10:22:48 +0100

    added constructor (title, author)

--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -7,6 +7,11 @@ public class Book {
     private String author;
     private boolean available;
 
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
     public String getId() {
         return id;
     }
